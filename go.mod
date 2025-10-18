18867649be8501a9d80586ad94a8f6f3d7f22fd7
ProstoLive <artem009saratov@gmail.com>
2025-10-18 19:49:01 +0400

    feat: Added service for yandex forms

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.2
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/rpc v1.2.1
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9
---
+++ services/yaforms.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"log"
+	"net/http"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc"
+	jsonrpc "github.com/gorilla/rpc/json"
+)
+
+type SendMessageArgs struct {
+	Aboba string `json:"aboba"`
+}
+
+type YaFormsService struct {
+	Bot *tgbotapi.BotAPI
+}
+
+func (y *YaFormsService) handleYaForms(r *http.Request, args *SendMessageArgs) error {
+	msg := tgbotapi.NewMessage(args.ChatID, args.Text)
+	_, err := y.Bot.Send(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateNewRpc(bot *tgbotapi.BotAPI) {
+	s := rpc.NewServer()
+	s.RegisterCodec(jsonrpc.NewCodec(), "application/json")
+	yfs := &YaFormsService{Bot: bot}
+	s.RegisterService(yfs, "Telegram")
+
+	router := mux.NewRouter()
+
+	router.Handle("/rpc", s)
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("HTTP server error: %v", err)
+	}
+}
